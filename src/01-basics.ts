8ab5dd84411eefb63ee2891bd8aa5915c404ac80
Yi Jie Lim <yijie.lim95@gmail.com>
2026-09-14 11:54:50 +0100

    Complete type-safety to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -56,7 +56,12 @@ const colleague3: Colleague = {
     extension: 125,
   },
 };
-const colleagues = {
+interface ColleagueHistory {
+  current: Colleague[],
+  former: Colleague[]
+}
+
+export const colleagues : ColleagueHistory = {
   current: [colleague1, colleague2, colleague3],
   former: [],
 };
