ba081279b0d7fd53eefc52135a7133942d5263f8
Yi Jie Lim <yijie.lim95@gmail.com>
2026-09-14 12:06:32 +0100

    Typed function signature.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,1 +1,41 @@
 import {Friend, Colleague } from './myTypes'
+import { friends, colleagues} from './01-basics'
+
+function older(f: Friend) : string {
+     f.age += 1
+     return `${f.name} is now ${f.age}`
+}
+
+console.log(older(friends[0]))
+
+// Find the colleague with the highest extension number.
+function highestExtension(cs: Colleague[]): Colleague {
+  const result = cs.sort(
+    (c1, c2) => c1.contact.extension - c2.contact.extension
+  );
+  return result[cs.length - 1];
+}
+console.log(highestExtension(colleagues.current));
+
+// Add a colleague, giving them the next available extension number.
+function addColleague(
+  cs: Colleague[],
+  name: string,
+  department: string,
+  email: string
+): Colleague {
+  const nextExtension = highestExtension(cs).contact.extension + 1;
+  const newColleague: Colleague = {
+    name,
+    department,
+    contact: {
+      email,
+      extension: nextExtension,
+    },
+  };
+  cs.push(newColleague);
+  return newColleague;
+}
+
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
