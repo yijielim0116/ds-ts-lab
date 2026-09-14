92a9e65b00c9220b1e988883a9d2da958999780d
Yi Jie Lim <yijie.lim95@gmail.com>
2026-09-14 12:40:01 +0100

    Define types using Utility types.

---
+++ src/05-utilityTypes.ts
@@ -0,0 +1,41 @@
+import { friends, colleagues } from "./01-basics";
+import { Friend, Colleague, SecureFriendContact, FriendPartial, EventPass } from "./myTypes";
+
+function updateFriend(friend: Friend, updates: FriendPartial ) : Friend {
+  return { ...friend, ...updates}
+}
+
+console.log(updateFriend(friends[0], {
+  phone: '08712345',
+  dob: new Date("1998-10-22")
+}))
+
+function generateEventPass(colleague: Colleague): EventPass {
+  const passCode = Math.round(Math.random() * (1000 - 1) + 1);
+  return {
+    name: colleague.name,
+    department: colleague.department,
+    passCode: passCode,
+  };
+}
+console.log(generateEventPass(colleagues.current[0]));
+
+function secureFindFriends(
+  friends: Friend[],
+  criteria: (f: Friend) => boolean
+): SecureFriendContact[] {
+  const matches = friends.filter(criteria);
+  return matches.map((f) => {
+    const secure: SecureFriendContact = {
+      name: f.name,
+      phone: f.phone,
+    };
+    return secure;
+  });
+}
+let result = secureFindFriends(
+    friends,
+    (f: Friend) => f.age < 30
+)
+console.log(result)
+result[0].phone = '08654321'
