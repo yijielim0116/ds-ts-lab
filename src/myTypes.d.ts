8d03ce6fa11dba3b93733ecbe2076a378cfb9341
Yi Jie Lim <yijie.lim95@gmail.com>
2026-09-14 12:23:08 +0100

    Demo optional properties and parameters.

--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -1,8 +1,11 @@
 export interface Friend {
     name: string;
     phone: string;
-    age: number
+    dob? : Date;   // New
+    age: number;
+    interests? : string[]   // New
 }
+
 
 export interface Colleague {
     name: string;
